f7efdcc83e766d54bd7851b96aeaa5a3defeccc2
JiteshJain123 <jiteshjain022@gmail.com>
2025-04-05 13:15:49 +0530

    Project Completed

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/SmartMart/",
   plugins: [react(),tailwindcss()],
+  base: "/SmartMart"
 })
